5d4a8475d524eb11b0e8399d093133cdc8734cf8
Nuran Nalci <nurannalci26@gmail.com>
2025-12-19 13:27:51 +0100

    Remove CvArticle type definition
    Removed CvArticle type definition from cv.ts.

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,11 +1,2 @@
-export type CvArticle = {
-  title: string;
-  org?: string;
-  location?: string;
-  period?: string;
-  bullets: string[];
-  tags?: string[];
-  type: 'work' | 'education';
-};
-
 export const cvArticles = [];
+
